5ad9b9986524d338fc9255c472b3a713dce9ec88
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-10-14 08:21:47 -0700

    XML skeleton

---
+++ AdventureCreatorAndroidProject/app/src/main/java/com/example/lmont/adventurecreator/StoryCreation.java
@@ -0,0 +1,13 @@
+package com.example.lmont.adventurecreator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StoryCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story_creation);
+    }
+}
---
+++ AdventureCreatorAndroidProject/app/src/main/java/com/example/lmont/adventurecreator/TransitionCreation.java
@@ -0,0 +1,13 @@
+package com.example.lmont.adventurecreator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TransitionCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition_creation);
+    }
+}
