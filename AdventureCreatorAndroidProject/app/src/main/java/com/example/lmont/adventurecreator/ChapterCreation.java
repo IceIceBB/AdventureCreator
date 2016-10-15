9aaf1e44cb5774bdf6946e795270fcdf7d4e15d4
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-10-15 13:39:33 -0700

    pull

---
+++ AdventureCreatorAndroidProject/app/src/main/java/com/example/lmont/adventurecreator/ChapterCreation.java
@@ -0,0 +1,13 @@
+package com.example.lmont.adventurecreator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChapterCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter_creation);
+    }
+}
