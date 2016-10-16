2bc894e0635f523f33ee0cf84f71d787fc7b17a6
pauldebellis <pvdebellis@gmail.com>
2016-10-16 15:28:43 -0700

    adds bookmark toggle to play activity

--- AdventureCreatorAndroidProject/app/src/main/java/com/example/lmont/adventurecreator/GamePlayActivity.java
+++ AdventureCreatorAndroidProject/app/src/main/java/com/example/lmont/adventurecreator/GamePlayActivity.java
@@ -2,12 +2,34 @@ package com.example.lmont.adventurecreator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 public class GamePlayActivity extends AppCompatActivity {
+
+    ViewSwitcher bookmark;
+    ImageView bookmarkHollow;
+    ImageView bookmarkSolid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
+
+        bookmark = (ViewSwitcher) findViewById(R.id.bookmark);
+        bookmarkHollow = (ImageView) findViewById(R.id.bookmarkHollow);
+        bookmarkSolid = (ImageView) findViewById(R.id.bookmarkSolid);
+
+        bookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bookmark.getCurrentView() != bookmarkHollow){
+                    bookmark.showPrevious();
+                }else if(bookmark.getCurrentView() != bookmarkSolid){
+                    bookmark.showNext();
+                }
+            }
+        });
     }
 }
