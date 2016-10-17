04657a13185339826cca5649def59687f2b48cde
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-10-17 11:39:32 -0700

    Chapter Creation mostly set (Committing to get Leo's updates.)

--- AdventureCreatorAndroidProject/app/src/main/java/com/example/lmont/adventurecreator/StoryCreation.java
+++ AdventureCreatorAndroidProject/app/src/main/java/com/example/lmont/adventurecreator/StoryCreation.java
@@ -9,22 +9,17 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-import com.android.volley.Response;
-
 import java.util.ArrayList;
-
-import static android.R.attr.id;
 
 public class StoryCreation extends AppCompatActivity {
     Button newStoryButton;
     ListView storyListView;
+
     Models.Story[] allStoriesArray;
     ArrayList<String> allStoryTitles;
     ArrayList<String> allStoryIds;
-    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-            this,
-            android.R.layout.simple_list_item_1,
-            allStoryTitles);
+
+    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, allStoryTitles);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,35 +29,29 @@ public class StoryCreation extends AppCompatActivity {
         newStoryButton = (Button) findViewById(R.id.newStoryButton);
         storyListView = (ListView) findViewById(R.id.storyListView);
 
+
 //        TODO: Add new story and pull for ID.
         newStoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                getAllTitlesAndIds();
                 Models.Story newStory = new Models.Story(
                         "Story " + allStoriesArray.length + 1,
                         "Story Summary",
                         "Story Genre",
                         "Story Author",
-                        "Story Tags"
-                );
-                Response.Listener<Models.Story> listener = new Response.Listener<>();
-                GameHelper.getInstance(StoryCreation.this).addStory(newStory, listener);
+                        "Story Tags");
+//                TODO: Fix these two so they aren't breaking the code (Something about Listener)
+//                Response.Listener<Models.Story> listener = new Response.Listener<>();
+//                GameHelper.getInstance(StoryCreation.this).addStory(newStory, listener);
+                getAllTitlesAndIds();
             }
         });
-//        TODO: Get data and populate list view with Story titles (additional info?)
-        allStoriesArray = GameHelper.getInstance(this).getAllStories();
-        for (int i = 0; i < allStoriesArray.length; i++) {
 
-            Models.Story storyAtI = allStoriesArray[i];
-            allStoryTitles.add(storyAtI.title);
-        }
+
+        getAllTitlesAndIds();
         storyListView.setAdapter(arrayAdapter);
 
-//        TODO: Use this to pass id for selected activity through on item click
-        for (int i = 0; i < allStoriesArray.length ; i++) {
-            Models.Story storyAtI = allStoriesArray[i];
-            allStoryIds.add(storyAtI._id);
-        }
 
 //        TODO: Transition to Chapter Creation with Story id as Intent Extra
         storyListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -70,13 +59,25 @@ public class StoryCreation extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
                 Intent intent = new Intent(StoryCreation.this, ChapterCreation.class);
 
-                intent.putExtra("selectedStoryId",allStoryIds.get(position));
+                intent.putExtra("selectedStoryId", allStoryIds.get(position));
                 startActivity(intent);
             }
         });
     }
 
 
+//    TODO: Get data and populate list view with Story titles (additional info?)
+    public void getAllTitlesAndIds() {
+        allStoriesArray = GameHelper.getInstance(this).getAllStories();
+
+        for (int i = 0; i < allStoriesArray.length; i++) {
+
+            Models.Story storyAtI = allStoriesArray[i];
+            allStoryTitles.add(storyAtI.title);
+//            TODO: Use this to pass id for selected activity through on item click
+            allStoryIds.add(storyAtI._id);
+        }
+    }
 
 
 }
